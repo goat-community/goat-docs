29cc50a6a40d4fbc4a1c8018e30826e9a857d708
majkshkurti <majkshkurti@outlook.com>
2024-10-24 10:47:06 +0200

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,20 +10,12 @@ const config = {
   title: "GOAT DOCS",
   tagline: "GOATs are cool",
   favicon: "img/favicon.ico",
-  // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: "https://goat.plan4better.de",
   baseUrl: "/docs/",
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "goat-community",
+  projectName: "goat",
   onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en", "de"],
